71d88bf2da408a9a2bf6078cb0b4347b4db69852
sei1002 <sei1002210@gmail.com>
2020-09-24 19:06:28 +0900

    作成したディスカッションルームへの遷移

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,25 @@
 class RoomsController < ApplicationController
+
   def new
     @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :messages 
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create, :show]
 end
