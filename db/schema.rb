366c598059e6f1abcb2eb65024a626c478408ff7
sei1002 <sei1002210@gmail.com>
2020-10-04 16:59:17 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -43,15 +43,6 @@ ActiveRecord::Schema.define(version: 2020_09_30_124239) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "room_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["room_id"], name: "index_room_users_on_room_id"
-    t.index ["user_id"], name: "index_room_users_on_user_id"
-  end
-
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "user_id"
@@ -76,7 +67,5 @@ ActiveRecord::Schema.define(version: 2020_09_30_124239) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
-  add_foreign_key "room_users", "rooms"
-  add_foreign_key "room_users", "users"
   add_foreign_key "rooms", "users"
 end
